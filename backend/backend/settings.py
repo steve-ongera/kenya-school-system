80e9ba638576b8e2dcb9568f1a9c9d01bee496f8
steve-ongabi <gadafimran411@gmail.com>
2026-09-04 14:27:09 +0300

    made updates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,12 +64,8 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # ---------------------------------------------------------------------------
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("DB_NAME", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("DB_USER", ""),
-        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
-        "HOST": os.environ.get("DB_HOST", ""),
-        "PORT": os.environ.get("DB_PORT", ""),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
